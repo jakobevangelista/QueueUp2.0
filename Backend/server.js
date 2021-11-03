c216f2c83cda6fb9228f3f55856b75b9555a8021
Aayush Bhattarai <barcaayush@Aayushs-MacBook-Pro.local>
2021-11-03 18:20:49 -0500

    Second Commit for all Endpoints included

--- Backend/server.js
+++ Backend/server.js
@@ -10,9 +10,21 @@ app.get('/', (req, res) => {
     res.send('this is working');
 })
 
-app.post('/', (req, res) =>{
+app.post('/twitterCall', (req, res) =>{
   res.send(twit.geTweet(req.body.keyword));
 });
+
+app.post('/IGDBCall', (req, res) => {
+  res.send('This is the endpoint for IGDB API');
+});
+
+app.post('/twitchCall', (req, res) => {
+  res.send('This is endpoint for twitch api');
+});
+
+app.post('/RAWGCall', (req, res) => {
+  res.send('This is endpoint for ')
+})
 
 app.listen(3000, ()=> {
     console.log("App is listening the local host");
