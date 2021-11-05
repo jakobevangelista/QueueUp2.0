6fb8f7eef3a013faf78f1bc2bf5429c347e50654
MarkHeadrick <mark.headrick53@gmail.com>
2021-11-05 17:37:26 -0500

    added controller for igdb

---
+++ Backend/controller/IGDBController.js
@@ -0,0 +1,78 @@
+
+// Get an app access token by making this request
+// POST https://id.twitch.tv/oauth2/token?client_id=yx9j15fb7kntnqe8xy0abmdfl04pnx&client_secret=0p3ltlqazwd0izn39idbrld8tmradq&grant_type=client_credentials
+
+var request = require('request');
+
+module.exports = (req,res) => {
+    function getGamesWithGenre(genre){
+        // retrieve the names for 10 games whose genre = genre
+        var options = {
+        'method': 'POST',
+        'url': 'https://api.igdb.com/v4/games/', // GAMES ENDPOINT
+        'headers': {
+            'Client-ID': 'yx9j15fb7kntnqe8xy0abmdfl04pnx',
+            'Authorization': 'Bearer bwpah7j4u5nzokow0itm08symaq0vp',
+            'Content-Type': 'text/plain'
+        },
+        body: 'fields *, cover.*; limit 10; where genres.name = ("' + genre + '") & rating > 0; sort rating desc;'
+        };
+    
+        // send request
+        var resp;
+        request(options, function (error, response) {
+        if (error) {
+            // throw new Error(error);
+            res.status(400).send("bad")
+        }
+        // parse response
+        resp = JSON.parse(response.body);
+        // console.log(resp); // print response
+        // var test = resp[0].cover;
+        // console.log(test.url);
+    
+        var finalData = [];
+        for (var i in resp) {
+            var coverJSON = resp[i].cover;
+            var name = resp[i].name;
+            var img = coverJSON.url;
+            // push the name and cover url of each game onto the final data array
+            finalData.push([name, img]); 
+        }
+    
+        // console.log(finalData);
+        res.status(200).send(arr)
+        });
+    }
+    
+    getGamesWithGenre("Strategy"); // genre name is case sensitive
+}
+
+
+module.exports = {getGamesWithGenre};
+
+
+// LIST of genres
+// "id": 2, "name": "Point-and-click"
+// "id": 4, "name": "Fighting"
+// "id": 5, "name": "Shooter"
+// "id": 7, "name": "Music"
+// "id": 8, "name": "Platform"
+// "id": 9, "name": "Puzzle"
+// "id": 10, "name": "Racing"
+// "id": 11, "name": "Real Time Strategy (RTS)"
+// "id": 12, "name": "Role-playing (RPG)"
+// "id": 13, "name": "Simulator"
+// "id": 14, "name": "Sport"
+// "id": 15, "name": "Strategy"
+// "id": 16, "name": "Turn-based strategy (TBS)"
+// "id": 24, "name": "Tactical"
+// "id": 25, "name": "Hack and slash/Beat 'em up"
+// "id": 26, "name": "Quiz/Trivia"
+// "id": 30, "name": "Pinball"
+// "id": 31, "name": "Adventure"
+// "id": 32, "name": "Indie"
+// "id": 33, "name": "Arcade"
+// "id": 34, "name": "Visual Novel"
+// "id": 35, "name": "Card & Board Game"
+// "id": 36, "name": "MOBA"
