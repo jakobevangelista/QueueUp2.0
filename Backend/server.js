55c5b00616fdfabf2c5fe706f50d93460e98bfc9
Martin Wnoroski <martin.wnorowski@tamu.edu>
2021-11-18 08:41:58 -0600

    Added new IGDB Controllers

--- Backend/server.js
+++ Backend/server.js
@@ -7,12 +7,16 @@ app.use(express.json());
 const RawgGenreController = require('./controller/RAWGGenreController.js');
 const TwitterController = require('./controller/TwitterController.js');
 const IGDBController = require('./controller/IGDBController.js');
+const IGDBkeySearch = require('./controller/IGDBsearchController.js');
+const IGDBrec = require('./controller/IGDBrecController.js');
 
 
 
 app.post('/twitterCall', TwitterController)
 app.post('/IGDBCall', IGDBController)
 app.post('/RAWGCall', RawgGenreController)
+app.post('/IGDBkeyCall', IGDBkeySearch)
+app.post('/IGDBrecCall', IGDBrec)
 
 app.listen(3000, ()=> {
     console.log("App is listening on local host");
