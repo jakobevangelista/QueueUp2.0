be98f669f6600bf8abc2185d99e3175ae92c5a1d
Aayush Bhattarai <barcaayush@Aayushs-MacBook-Pro.local>
2021-11-21 00:01:30 -0600

    Twitter API v2 with filters

--- Backend/controller/TwitterController.js
+++ Backend/controller/TwitterController.js
@@ -5,10 +5,11 @@ require('dotenv').config();
 module.exports = (req,res) => {
     var query = req.body.keyword;
     var newQuery = query.replace(':', ' ');
+
     console.log(newQuery);
     var options = {
         'method': 'GET',
-        'url': 'https://api.twitter.com/2/tweets/search/recent?query='+ newQuery,
+        'url': 'https://api.twitter.com/2/tweets/search/recent?query='+ newQuery + '%20lang%3Aen%20-has%3Amedia%20-has%3Alinks%20has%3Ahashtags',
         'headers': {
           'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAABYhVQEAAAAACQjzSUb8KT3VG1nQgJdccQED%2F3E%3D9xRG40ob5IvMQIzuCuY8oU55jkqnAoqq1xWJx5zu7hSVxtuLMH',
         }
