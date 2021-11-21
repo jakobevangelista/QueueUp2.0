841cedae4220304cb0fa1c311a93b545278439ac
Aayush Bhattarai <barcaayush@Aayushs-MBP.attlocal.net>
2021-11-21 15:29:06 -0600

    minor changes on filter and add terraform file for infrastructure

---
+++ terraform/provider.tf
@@ -0,0 +1,5 @@
+#configuration of heroku provider
+provider "heroku{
+    email = "barcaayush@tamu.edu"
+    api_key = 
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable hero_api_key{
+    type = "string"
+    default = " "
+}
+
+resource "heroku_app" "default" {
+
+}
